668fdc868ea8af42dd1ae76eccb0e9b1a02c2ba4
joechea <s.chea@aupp.edu.kh>
2024-05-16 18:09:53 +0700

    initial commit

---
+++ calculator.mjs
@@ -0,0 +1,21 @@
+export function substract(a, b) {
+  return a - b;
+}
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function multiply(a, b) {
+  return a * b;
+}
+
+export function divide(a, b) {
+  return a / b;
+}
+
+const main = () => {
+  console.log("Hello from main function");
+};
+
+export default main;
---
+++ index.mjs
@@ -0,0 +1,5 @@
+import index, { add, substract } from "./calculator.mjs";
+
+index();
+console.log(add(1, 2));
+console.log(substract(1, 2));
